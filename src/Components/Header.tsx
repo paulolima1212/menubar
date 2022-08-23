93acb2ab86860ea6289e1d3963e0db1c0f8837be
paulolima1212 <plima12121984@gmail.com>
2022-08-23 19:21:01 +0100

    adjustment css

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <div className="header">
             <h1>Menu Bar</h1>
-            <img src="../src/image/logo.png" alt="logowheelsbar" className="logoimage"/>
+            <img src="src/image/logo.png" alt="logowheelsbar" className="logoimage"/>
         </div>
     )
 }
--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -12,6 +12,7 @@ export default function Modal() {
             <div className="frame-modal">
                 <h3 className="title-modal">{modalContent.title}</h3>
                 <img src={modalContent.src} alt="drink-pinacolada" />
+                <div className="line-separator"></div>
                 <p className="desc-modal">{modalContent.content}</p>
                 <input type="button" value="Close" onClick={() => handleCloseModal()} />
             </div>
